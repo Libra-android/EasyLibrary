9899c9972fe18f369bd0252faf67faec79a12749
1187968243@qq.com <1187968243@qq.com>
2020-04-21 17:34:23 +0800

    add

--- lib/src/main/java/com/easy/lib/encryption/password.kt
+++ lib/src/main/java/com/easy/lib/encryption/password.kt
@@ -8,27 +8,26 @@ import java.security.NoSuchAlgorithmException
  * @date  2020/4/21 16:30
  */
 
-object password{
 
-    fun String.md5String(): String{
-        try {
-            val instance: MessageDigest = MessageDigest.getInstance("MD5")//获取md5加密对象
-            val digest:ByteArray = instance.digest(toByteArray())//对字符串加密，返回字节数组
-            var sb : StringBuffer = StringBuffer()
-            for (b in digest) {
-                var i :Int = b.toInt() and 0xff//获取低八位有效值
-                var hexString = Integer.toHexString(i)//将整数转化为16进制
-                if (hexString.length < 2) {
-                    hexString = "0" + hexString//如果是一位的话，补0
-                }
-                sb.append(hexString)
+fun String.md5String(): String {
+    try {
+        val instance: MessageDigest = MessageDigest.getInstance("MD5")//获取md5加密对象
+        val digest: ByteArray = instance.digest(toByteArray())//对字符串加密，返回字节数组
+        var sb: StringBuffer = StringBuffer()
+        for (b in digest) {
+            var i: Int = b.toInt() and 0xff//获取低八位有效值
+            var hexString = Integer.toHexString(i)//将整数转化为16进制
+            if (hexString.length < 2) {
+                hexString = "0" + hexString//如果是一位的话，补0
             }
-            return sb.toString()
-
-        } catch (e: NoSuchAlgorithmException) {
-            e.printStackTrace()
+            sb.append(hexString)
         }
-        return ""
-    }
+        return sb.toString()
 
+    } catch (e: NoSuchAlgorithmException) {
+        e.printStackTrace()
+    }
+    return ""
 }
+
+
